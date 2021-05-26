ead1eacc1e2f1beeb2ca61839ffb087ccc14583a
Civa <44102586+civa@users.noreply.github.com>
2021-05-26 15:04:57 +0100

    Update config.ts

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -26,15 +26,16 @@ const config: MenuEntry[] = [
     href: '/farms',
   },
   {
-     label: 'Partnership',
-     icon: 'NftIcon',
-     href: 'https://forms.gle/hHs52znCgrotJUTNA',
-   },
-  {
      label: 'Bridge',
      icon: 'SunIcon',
      href: 'https://bscbridge.corion.io/',
    },
+  {
+     label: 'Partnership',
+     icon: 'NftIcon',
+     href: 'https://forms.gle/hHs52znCgrotJUTNA',
+   },
+  
   // {
   //  label: 'poolss',
   //  icon: 'PoolIcon',
@@ -60,12 +61,8 @@ const config: MenuEntry[] = [
     icon: 'MoreIcon',
     items: [
       {
-        label: 'Github',
-        href: 'https://github.com/CORIONplatform',
-      },
-      {
-        label: 'Bscscan',
-        href: 'https://bscscan.com/token/0x141383CDB8158982fB3469C3e49cC82F8026d968',
+        label: 'Website',
+        href: 'https://corion.io/',
       },
       {
         label: 'CoinMarketCap',
@@ -75,10 +72,15 @@ const config: MenuEntry[] = [
         label: 'CoinGecko',
         href: 'https://www.coingecko.com/en/coins/corionx',
       },
+      
       {
-        label: 'Webpage',
-        href: 'https://corion.io/',
+        label: 'Bscscan',
+        href: 'https://bscscan.com/token/0x141383CDB8158982fB3469C3e49cC82F8026d968',
       },
+      {
+        label: 'Github',
+        href: 'https://github.com/CORIONplatform',
+      },  
     ],
   },
   //  {
