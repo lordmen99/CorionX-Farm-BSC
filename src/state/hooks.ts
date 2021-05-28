a32f6878086b312d347951a5a118e4e65f82e57f
Civa <44102586+civa@users.noreply.github.com>
2021-05-28 08:02:05 +0100

    Update hooks.ts

--- src/state/hooks.ts
+++ src/state/hooks.ts
@@ -78,7 +78,7 @@ export const usePriceCakeBusd = (): BigNumber => {
   // const bnbPriceUSD = usePriceBnbBusd()
   // const farm = useFarmFromPid(pid)
   // return farm.tokenPriceVsQuote ? bnbPriceUSD.times(farm.tokenPriceVsQuote) : ZERO
-  const pid = 0 // CORX-BUSD LP
+  const pid = 3 // CORX-BUSD LP
   const farm = useFarmFromPid(pid)
   return farm.tokenPriceVsQuote ? new BigNumber(farm.tokenPriceVsQuote) : ZERO
 }
